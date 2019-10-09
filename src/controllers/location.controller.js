dc47028b17f41d0f1bc848e0d14b1cea144ceebc
shiv <shivssharma1990@gmail.com>
2019-10-09 12:00:40 +0530

    location bulk upload

--- src/controllers/location.controller.js
+++ src/controllers/location.controller.js
@@ -58,41 +58,40 @@ module.exports.addBulkLocation = (req, res) => {
     });
   }
 
-  fs.writeFile(
-    "src/temp/temp.xlsx",
-    req.body.file,
-    { encoding: "base64" },
-    function(err) {
-      if (err) {
-        return res.status(500).send({
-          error: true,
-          message: "Error while creating xlsx file",
-          data: err
-        });
-      } else {
-        const workbook = xlsx.readFile("src/temp/temp.xlsx");
-        const sheetData = xlsx.utils.sheet_to_json(
-          workbook.Sheets[workbook.SheetNames[0]]
-        );
+  let file = req.body.file.split(";base64,")[1];
 
-        sheetData.forEach(sheetData => {
-          const Location = new locationModel({
-            _id: Math.floor(100000 + Math.random() * 900000),
-            name: sheetData.name
-          });
+  fs.writeFile("src/temp/temp.xlsx", file, { encoding: "base64" }, function(
+    err
+  ) {
+    if (err) {
+      return res.status(500).send({
+        error: true,
+        message: "Error while creating xlsx file",
+        data: err
+      });
+    } else {
+      const workbook = xlsx.readFile("src/temp/temp.xlsx");
+      const sheetData = xlsx.utils.sheet_to_json(
+        workbook.Sheets[workbook.SheetNames[0]]
+      );
 
-          Location.save(err => {
-            console.log(err);
-          });
+      sheetData.forEach(sheet => {
+        const Location = new locationModel({
+          _id: Math.floor(100000 + Math.random() * 900000),
+          name: sheet.name
         });
 
-        res.send({
-          error: false,
-          message: "Locations saved successfully"
+        location.save(err => {
+          console.log("saved");
         });
-      }
+      });
+
+      res.send({
+        error: false,
+        message: "Locations saved successfully"
+      });
     }
-  );
+  });
 };
 
 module.exports.getAllLocations = (req, res) => {
