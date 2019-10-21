5227a5afe3be43ce18e79e262e75c83bdfa6913c
shiv <shivssharma1990@gmail.com>
2019-10-21 15:43:33 +0530

    other referal issue resolved

--- src/controllers/refer.controller.js
+++ src/controllers/refer.controller.js
@@ -43,52 +43,78 @@ module.exports.addRefer = async (req, res) => {
 
   const jobAssociated = await jobModel.findById(req.body.jobId);
 
-  // check if referal already exists or not
-  referModel.findOne(
-    {
-      jobId: req.body.jobId,
-      $or: [{ email: req.body.email }, { mobile: req.body.email }]
-    },
-    (err, referalFound) => {
+  if (jobAssociated) {
+    referModel.findOne(
+      {
+        jobId: req.body.jobId,
+        $or: [{ email: req.body.email }, { mobile: req.body.email }]
+      },
+      (err, referalFound) => {
+        if (err) {
+          return res.status(500).send({
+            error: true,
+            message: "Error while finding referal",
+            data: err
+          });
+        } else if (referalFound) {
+          return res.status(400).send({
+            error: true,
+            message: "Candidate already refered !"
+          });
+        } else {
+          const refer = new referModel({
+            jobId: req.body.jobId,
+            referBy: req.userId,
+            assignedTo: jobAssociated.postBy,
+            name: req.body.name,
+            email: req.body.email,
+            mobile: req.body.mobile,
+            resume: req.body.resume
+          });
+
+          refer.save((err, referSaved) => {
+            if (err) {
+              return res.status(500).send({
+                error: true,
+                message: "Error while saving referal",
+                data: err
+              });
+            } else {
+              return res.status(200).send({
+                error: false,
+                message: "Referal saved successfully"
+              });
+            }
+          });
+        }
+      }
+    );
+  } else {
+    const refer = new referModel({
+      jobId: null,
+      referBy: req.userId,
+      assignedTo: null,
+      name: req.body.name,
+      email: req.body.email,
+      mobile: req.body.mobile,
+      resume: req.body.resume
+    });
+
+    refer.save((err, referSaved) => {
       if (err) {
         return res.status(500).send({
           error: true,
-          message: "Error while finding referal",
+          message: "Error while saving referal",
           data: err
         });
-      } else if (referalFound) {
-        return res.status(400).send({
-          error: true,
-          message: "Candidate already refered !"
-        });
       } else {
-        const refer = new referModel({
-          jobId: req.body.jobId,
-          referBy: req.userId,
-          assignedTo: jobAssociated.postBy,
-          name: req.body.name,
-          email: req.body.email,
-          mobile: req.body.mobile,
-          resume: req.body.resume
-        });
-
-        refer.save((err, referSaved) => {
-          if (err) {
-            return res.status(500).send({
-              error: true,
-              message: "Error while saving referal",
-              data: err
-            });
-          } else {
-            return res.status(200).send({
-              error: false,
-              message: "Referal saved successfully"
-            });
-          }
+        return res.status(200).send({
+          error: false,
+          message: "Referal saved successfully"
         });
       }
-    }
-  );
+    });
+  }
 };
 
 module.exports.updateReferalStatus = (req, res) => {
